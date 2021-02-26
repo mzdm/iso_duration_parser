a0ffcab75ef7cb58a020ee81b2cfd72b8dbdc626
Matej Zidek <zidek.matt@gmail.com>
2021-02-26 17:17:58 +0100

    lib changes

--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -1,47 +1,89 @@
 const _pattern =
     r'^P(?!$)(\d+(?:\.\d+)?Y)?(\d+(?:\.\d+)?M)?(\d+(?:\.\d+)?W)?(\d+(?:\.\d+)?D)?(T(?=\d)(\d+(?:\.\d+)?H)?(\d+(?:\.\d+)?M)?(\d+(?:[\.,\,]\d+)?S)?)?$';
 
+/// [IsoDuration] - ISO 8061 Duration Data Type
+///
+/// The duration data type is used to specify a time interval.
+///
+/// This format is often used as response value in
+/// REST APIs (YouTube video duration or length of the flight).
+///
+/// The time interval is specified in the following form `PnYnMnDTnHnMnS` where:
+///
+/// - P indicates the period (required)
+/// - nY indicates the number of years
+/// - nM indicates the number of months
+/// - nD indicates the number of days
+/// - T indicates the start of a time section
+/// (required if you are going to specify hours, minutes, or seconds)
+/// - nH indicates the number of hours
+/// - nM indicates the number of minutes
+/// - nS indicates the number of seconds
+///
+///
+/// Example:
+///
+/// ```dart
+/// IsoDuration.parse('P5Y'); // IsoDuration{years: 5, months: 0, weeks: 0, days: 0, hours: 0, minutes: 0, seconds: 0};
+/// IsoDuration.parse('P3Y6M4DT12H30M5S'); // IsoDuration{years: 3, months: 6, weeks: 0, days: 4, hours: 12, minutes: 30, seconds: 5};
+///
+/// can parse also decimal:
+/// IsoDuration.parse('PT8M40.215S'); // IsoDuration{years: 0, months: 0, weeks: 0, days: 0, hours: 0, minutes: 8, seconds: 40.215};
+/// ```
+///
+/// **Note**: This [IsoDuration] is not like Dart's implementation of [Duration].
+///
+/// Despite the similar name, a [Duration] object does not implement "Durations"
+/// as specified by ISO 8601. In particular, a duration object does not keep track
+/// of the individually provided members (such as "days" or "hours"),
+/// but only uses these arguments to compute the length of
+/// the corresponding time interval.
+///
+/// See also:
+///
+///  * [Wikipedia, ISO_8601 Durations](https://en.wikipedia.org/wiki/ISO_8601#Durations)
+///  * [XSD Duration Data Type](https://www.w3schools.com/xml/schema_dtypes_date.asp)
 class IsoDuration {
   /// The duration part in years, if was given, otherwise defaults to 0.
-  final double inYears;
+  final double years;
 
   /// The duration part in months, if was given, otherwise defaults to 0.
-  final double inMonths;
+  final double months;
 
   /// The duration part in weeks, if was given, otherwise defaults to 0.
-  final double inWeeks;
+  final double weeks;
 
   /// The duration part in days, if was given, otherwise defaults to 0.
-  final double inDays;
+  final double days;
 
   /// The duration part in hours, if was given, otherwise defaults to 0.
-  final double inHours;
+  final double hours;
 
   /// The duration part in minutes, if was given, otherwise defaults to 0.
-  final double inMinutes;
+  final double minutes;
 
   /// The duration part in seconds, if was given, otherwise defaults to 0.
-  final double inSeconds;
+  final double seconds;
 
   const IsoDuration({
-    this.inYears = 0,
-    this.inMonths = 0,
-    this.inWeeks = 0,
-    this.inDays = 0,
-    this.inHours = 0,
-    this.inMinutes = 0,
-    this.inSeconds = 0,
+    this.years = 0,
+    this.months = 0,
+    this.weeks = 0,
+    this.days = 0,
+    this.hours = 0,
+    this.minutes = 0,
+    this.seconds = 0,
   });
 
-  /// Returns `true` if all values of [IsoDuration] object are 0.
+  /// Returns `true` if all values of [IsoDuration] object are equal to 0.
   bool get isZero => <double>[
-        inYears,
-        inMonths,
-        inWeeks,
-        inDays,
-        inHours,
-        inMinutes,
-        inSeconds
+        years,
+        months,
+        weeks,
+        days,
+        hours,
+        minutes,
+        seconds
       ].every((element) => element == 0);
 
   /// Parses the ISO 8601 - Duration. If the operation was not successful then
@@ -50,17 +92,17 @@ class IsoDuration {
   /// Usage example:
   ///
   /// ```dart
-  /// IsoDuration.parse('P5Y'); // IsoDuration{inYears: 5, inMonths: 0, inWeeks: 0, inDays: 0, inHours: 0, inMinutes: 0, inSeconds: 0};
-  /// IsoDuration.parse('P3Y6M4DT12H30M5S'); // IsoDuration{inYears: 3, inMonths: 6, inWeeks: 0, inDays: 4, inHours: 12, inMinutes: 30, inSeconds: 5};
+  /// IsoDuration.parse('P5Y'); // IsoDuration{years: 5, months: 0, weeks: 0, days: 0, hours: 0, minutes: 0, seconds: 0};
+  /// IsoDuration.parse('P3Y6M4DT12H30M5S'); // IsoDuration{years: 3, months: 6, weeks: 0, days: 4, hours: 12, minutes: 30, seconds: 5};
   ///
   /// can parse also decimal:
-  /// IsoDuration.parse('PT8M40.215S'); // IsoDuration{inYears: 0, inMonths: 0, inWeeks: 0, inDays: 0, inHours: 0, inMinutes: 8, inSeconds: 40.215};
+  /// IsoDuration.parse('PT8M40.215S'); // IsoDuration{years: 0, months: 0, weeks: 0, days: 0, hours: 0, minutes: 8, seconds: 40.215};
   /// ```
   ///
   /// See also:
   ///
   ///  * [Wikipedia, ISO_8601 Durations](https://en.wikipedia.org/wiki/ISO_8601#Durations)
-  ///  * [XML Schema 1.0 xsd:duration](http://www.datypic.com/sc/xsd/t-xsd_duration.html)
+  ///  * [XSD Duration Data Type](https://www.w3schools.com/xml/schema_dtypes_date.asp)
   static IsoDuration parse(String input) {
     final parsed = tryParse(input);
     if (parsed != null) {
@@ -73,72 +115,60 @@ class IsoDuration {
   /// returns `null` if the [input] is invalid.
   static IsoDuration? tryParse(String input) {
     final regExp = RegExp(_pattern);
-    Match? matches;
-    try {
-      matches = regExp.matchAsPrefix(input);
-    } catch (e) {
-      print(e);
-    }
+    final matches = regExp.matchAsPrefix(input);
+
     if (matches != null) {
+      final y = matches.group(1)?.replaceFirst('Y', '')._replaceComma() ?? '';
+      final m = matches.group(2)?.replaceFirst('M', '')._replaceComma() ?? '';
+      final w = matches.group(3)?.replaceFirst('W', '')._replaceComma() ?? '';
+
+      final d = matches.group(4)?.replaceFirst('D', '')._replaceComma() ?? '';
+      final hrs = matches.group(6)?.replaceFirst('H', '')._replaceComma() ?? '';
+      final min = matches.group(7)?.replaceFirst('M', '')._replaceComma() ?? '';
+      final sec = matches.group(8)?.replaceFirst('S', '')._replaceComma() ?? '';
+
       return IsoDuration(
-          inYears: double.tryParse(matches
-                      .group(1)
-                      ?.replaceFirst('Y', '')
-                      .replaceFirst(',', '.') ??
-                  '') ??
-              0,
-          inMonths: double.tryParse(matches
-                      .group(2)
-                      ?.replaceFirst('M', '')
-                      .replaceFirst(',', '.') ??
-                  '') ??
-              0,
-          inWeeks: double.tryParse(
-                  matches.group(3)?.replaceFirst('W', '').replaceFirst(',', '.') ??
-                      '') ??
-              0,
-          inDays:
-              double.tryParse(matches.group(4)?.replaceFirst('D', '').replaceFirst(',', '.') ?? '') ?? 0,
-          inHours: double.tryParse(matches.group(6)?.replaceFirst('H', '').replaceFirst(',', '.') ?? '') ?? 0,
-          inMinutes: double.tryParse(matches.group(7)?.replaceFirst('M', '').replaceFirst(',', '.') ?? '') ?? 0,
-          inSeconds: double.tryParse(matches.group(8)?.replaceFirst('S', '').replaceFirst(',', '.') ?? '') ?? 0);
+        years: double.tryParse(y) ?? 0,
+        months: double.tryParse(m) ?? 0,
+        weeks: double.tryParse(w) ?? 0,
+        days: double.tryParse(d) ?? 0,
+        hours: double.tryParse(hrs) ?? 0,
+        minutes: double.tryParse(min) ?? 0,
+        seconds: double.tryParse(sec) ?? 0,
+      );
     }
     return null;
   }
 
-  @override
-  String toString() =>
-      'IsoDuration{inYears: $inYears, inMonths: $inMonths, inWeeks: $inWeeks, inDays: $inDays, inHours: $inHours, inMinutes: $inMinutes, inSeconds: $inSeconds}';
+  /// Calculates total duration only in seconds from the [IsoDuration] object.
+  /// It is a sum of all individual parts, except [years] and [months].
+  ///
+  /// **Note:** values `years` and `months` of [IsoDuration] must be equal to 0.
+  /// Otherwise, it is not possible to accurately count the total of seconds.
+  ///
+  /// For example:
+  ///
+  /// ```dart
+  /// final dur = IsoDuration(hours: 1, minutes: 2, seconds: 5.5)
+  /// dur.toSeconds(); // 3725.5
+  /// ```
+  double toSeconds() {
+    assert(years == 0 && months == 0,
+        'years and months values of the IsoDuration object must be 0!');
+    const secsInDay = 86400;
+    const secsInHour = 3600;
 
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is IsoDuration &&
-          runtimeType == other.runtimeType &&
-          inYears == other.inYears &&
-          inMonths == other.inMonths &&
-          inWeeks == other.inWeeks &&
-          inDays == other.inDays &&
-          inHours == other.inHours &&
-          inMinutes == other.inMinutes &&
-          inSeconds == other.inSeconds;
+    final weeksInSecs = weeks * secsInDay * 7;
+    final daysInSecs = days * secsInDay;
+    final hrsInSecs = hours * secsInHour;
+    final minsInSecs = minutes * 60;
+    return weeksInSecs + daysInSecs + hrsInSecs + minsInSecs + seconds;
+  }
 
-  @override
-  int get hashCode =>
-      inYears.hashCode ^
-      inMonths.hashCode ^
-      inWeeks.hashCode ^
-      inDays.hashCode ^
-      inHours.hashCode ^
-      inMinutes.hashCode ^
-      inSeconds.hashCode;
-}
-
-extension IsoDurationExt on IsoDuration {
   /// TODO: Unimplemented: Possibility to convert back to ISO
   ///
   /// This method returns a [String] value from the [IsoDuration] object in
-  /// ISO 8601 - Duration format (`PnYnMnDTnHnMnS`).
+  /// ISO 8601 - Duration (`PnYnMnDTnHnMnS` format).
   ///
   /// If any part is zero then it is omitted. If all parts are zero then it
   /// returns `PT0S`.
@@ -146,26 +176,39 @@ extension IsoDurationExt on IsoDuration {
   /// See also:
   ///
   ///  * [Wikipedia, ISO_8601 Durations](https://en.wikipedia.org/wiki/ISO_8601#Durations)
-  ///  * [XML Schema 1.0 xsd:duration](http://www.datypic.com/sc/xsd/t-xsd_duration.html)
-  String _toIso() {
-    throw 'Unimplemented method call';
+  ///  * [XSD Duration Data Type](https://www.w3schools.com/xml/schema_dtypes_date.asp)
+  void toIso() {
+    print('Unimplemented method call: toIso() is not yet implemented.');
   }
 
-  /// TODO: Unimplemented: Convert total to seconds
-  /// Calculates total duration only in seconds from the [IsoDuration] object.
-  ///
-  /// **Note:** values `inYears` and `inMonths` of [IsoDuration] must be 0.
-  /// Otherwise, it is not possible to accurately count the total of seconds.
-  ///
-  /// For example:
-  ///
-  /// ```dart
-  /// final dur = IsoDuration(inHours: 1, inMinutes: 2, inSeconds: 5.5)
-  /// dur.toSeconds(); // 3725.5
-  /// ```
-  double _toSeconds(IsoDuration isoDuration) {
-    assert(isoDuration.inYears == 0 && isoDuration.inMonths == 0,
-        'inYears and inMonths values of the IsoDuration object must be 0!');
-    throw 'Unimplemented method call';
-  }
+  @override
+  String toString() =>
+      'IsoDuration{years: $years, months: $months, weeks: $weeks, days: $days, hours: $hours, minutes: $minutes, seconds: $seconds}';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is IsoDuration &&
+          runtimeType == other.runtimeType &&
+          years == other.years &&
+          months == other.months &&
+          weeks == other.weeks &&
+          days == other.days &&
+          hours == other.hours &&
+          minutes == other.minutes &&
+          seconds == other.seconds;
+
+  @override
+  int get hashCode =>
+      years.hashCode ^
+      months.hashCode ^
+      weeks.hashCode ^
+      days.hashCode ^
+      hours.hashCode ^
+      minutes.hashCode ^
+      seconds.hashCode;
+}
+
+extension IsoDurationStringExt on String {
+  String _replaceComma() => replaceFirst(',', '.');
 }
