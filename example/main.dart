7d3303e7837046f601c8c8ddae9326bf2a847a9f
Matej Zidek <zidek.matt@gmail.com>
2021-06-16 15:56:44 +0200

    chore: fix formatting

--- example/main.dart
+++ example/main.dart
@@ -21,13 +21,16 @@ void main() {
   dur2.toSeconds(); // 45054.5
   dur2.toIso(); // 'PT12H30M50.4S'
 
-  final dur3 = IsoDuration.parse('P5Y'); // IsoDuration{years: 5, months: 0, weeks: 0, days: 0, hours: 0, minutes: 0, seconds: 0};
+  final dur3 = IsoDuration.parse('P5Y');
+  // equals to: IsoDuration(years: 5, months: 0, weeks: 0, days: 0, hours: 0, minutes: 0, seconds: 0)
   //dur3.toSeconds(); // assertion error, years and months must be equal to 0
 
-  IsoDuration.parse('P3Y6M4DT12H30M5S'); // IsoDuration{years: 3, months: 6, weeks: 0, days: 4, hours: 12, minutes: 30, seconds: 5};
+  IsoDuration.parse('P3Y6M4DT12H30M5S');
+  // equals to IsoDuration(years: 3, months: 6, weeks: 0, days: 4, hours: 12, minutes: 30, seconds: 5)
 
   // can parse decimal (accepts both comma and dots):
-  IsoDuration.parse('PT8M40.215S'); // IsoDuration{years: 0, months: 0, weeks: 0, days: 0, hours: 0, minutes: 8, seconds: 40.215};
+  IsoDuration.parse('PT8M40.215S');
+  // equals to IsoDuration(years: 0, months: 0, weeks: 0, days: 0, hours: 0, minutes: 8, seconds: 40.215)
 
   try {
     IsoDuration.parse('P 50M');
